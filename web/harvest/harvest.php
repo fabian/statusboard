a28777bc86d3dd864b441e6f10c81d00f8061f0b
Fabian Vogler <fabian@equivalence.ch>
2013-10-29 14:24:54 +0100

    Add authentication to server URL

--- web/harvest/harvest.php
+++ web/harvest/harvest.php
@@ -4,7 +4,18 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 $config = require __DIR__ . '/config.php';
 
-$config['server_url'] = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']);
+$config['server_url'] = 'http://';
+if (isset($_SERVER['PHP_AUTH_USER'])) {
+    $config['server_url'] .= $_SERVER['PHP_AUTH_USER'];
+    if (isset($_SERVER['PHP_AUTH_PW'])) {
+        $config['server_url'] .= ':';
+        $config['server_url'] .= $_SERVER['PHP_AUTH_PW'];
+    }
+    $config['server_url'] .= '@';
+}
+$config['server_url'] .= $_SERVER['HTTP_HOST'];
+$config['server_url'] .= dirname($_SERVER['SCRIPT_NAME']);
+
 $config['redirect_uri'] = $config['server_url'] . '/callback.php';
 
 $client = new Guzzle\Http\Client($config['base_url']);
