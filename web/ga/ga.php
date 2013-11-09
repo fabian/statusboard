b245d682a33b2ba775a46449ee8aad2f7c780826
Fabian Vogler <fabian@equivalence.ch>
2013-11-09 18:06:00 +0100

    Exclude auth from redirect_uri

--- web/ga/ga.php
+++ web/ga/ga.php
@@ -4,19 +4,19 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 $config = require __DIR__ . '/config.php';
 
-$config['server_url'] = 'http://';
+$auth = '';
 if (isset($_SERVER['PHP_AUTH_USER'])) {
-    $config['server_url'] .= $_SERVER['PHP_AUTH_USER'];
+    $auth .= $_SERVER['PHP_AUTH_USER'];
     if (!empty($_SERVER['PHP_AUTH_PW'])) {
-        $config['server_url'] .= ':';
-        $config['server_url'] .= $_SERVER['PHP_AUTH_PW'];
+        $auth .= ':';
+        $auth .= $_SERVER['PHP_AUTH_PW'];
     }
-    $config['server_url'] .= '@';
+    $auth .= '@';
 }
-$config['server_url'] .= $_SERVER['HTTP_HOST'];
-$config['server_url'] .= dirname($_SERVER['SCRIPT_NAME']);
+$server = $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']);
 
-$config['redirect_uri'] = $config['server_url'] . '/callback.php';
+$config['server_url'] = 'http://' . $auth . $server . '/callback.php';
+$config['redirect_uri'] = 'http://' . $server . '/callback.php';
 
 $authClient = new Guzzle\Http\Client($config['auth_url']);
 $client = new Guzzle\Http\Client($config['base_url']);
