75f522232ef7995e0d2d2ccf06a82d908538f460
Fabian Vogler <fabian@equivalence.ch>
2014-09-26 19:02:16 +0200

    Remove GA Lucerne

--- web/countdown/index.php
+++ web/countdown/index.php
@@ -1,9 +1,5 @@
 <table>
     <tr>
-        <td>Lucerne</td>
-        <td style="width: 160px"><?php $date = strtotime('2014-09-12'); echo ceil(($date - time())/60/60/24); ?> days</td>
-    </tr>
-    <tr>
         <td>São Paulo</td>
         <td style="width: 160px"><?php $date = strtotime('2015-08-11'); echo ceil(($date - time())/60/60/24); ?> days</td>
     </tr>
