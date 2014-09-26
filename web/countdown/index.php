05eeed36a8f4d26913918aa2d6a68ba1fd39d31e
Fabian Vogler <fabian@equivalence.ch>
2014-09-26 19:02:30 +0200

    Add GA Niagara Falls

--- web/countdown/index.php
+++ web/countdown/index.php
@@ -4,6 +4,10 @@
         <td style="width: 160px"><?php $date = strtotime('2015-08-11'); echo ceil(($date - time())/60/60/24); ?> days</td>
     </tr>
     <tr>
+        <td>Niagara Falls</td>
+        <td style="width: 160px"><?php $date = strtotime('2016-10-01'); echo ceil(($date - time())/60/60/24); ?> days</td>
+    </tr>
+    <tr>
         <td>Abu Dhabi</td>
         <td style="width: 160px"><?php $date = strtotime('2017-10-17'); echo ceil(($date - time())/60/60/24); ?> days</td>
     </tr>
