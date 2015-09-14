aa6c57a6e8b57cbdf6aec2f8d308049c84a3cdf1
Fabian Vogler <fabian@equivalence.ch>
2015-09-14 15:38:21 +0200

    Add WSC2019

--- web/countdown/index.php
+++ web/countdown/index.php
@@ -11,4 +11,8 @@
         <td>Abu Dhabi</td>
         <td style="width: 160px"><?php $date = strtotime('2017-10-14'); echo ceil(($date - time())/60/60/24); ?> days</td>
     </tr>
+    <tr>
+        <td>Kazan</td>
+        <td style="width: 160px"><?php $date = strtotime('2017-08-18'); echo ceil(($date - time())/60/60/24); ?> days</td>
+    </tr>
 </table>
