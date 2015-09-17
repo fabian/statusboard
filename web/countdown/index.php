a08b755c3fc3903166472e7e7df04744641cde9d
Fabian Vogler <fabian@equivalence.ch>
2015-09-17 08:16:46 +0200

    Remove WSC2015

--- web/countdown/index.php
+++ web/countdown/index.php
@@ -1,9 +1,5 @@
 <table>
     <tr>
-        <td>S&#227;o Paulo</td>
-        <td style="width: 160px"><?php $date = strtotime('2015-08-11'); echo ceil(($date - time())/60/60/24); ?> days</td>
-    </tr>
-    <tr>
         <td>Niagara Falls</td>
         <td style="width: 160px"><?php $date = strtotime('2016-10-01'); echo ceil(($date - time())/60/60/24); ?> days</td>
     </tr>
