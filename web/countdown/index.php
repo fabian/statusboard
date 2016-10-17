492a9e7ff3201bb87439531077e7c2800e06f59e
Fabian Vogler <fabian@equivalence.ch>
2016-10-17 06:55:27 +0200

    Add GA2018

--- web/countdown/index.php
+++ web/countdown/index.php
@@ -1,10 +1,14 @@
 <table>
     <tr>
         <td>Abu Dhabi</td>
-        <td style="width: 160px"><?php $date = strtotime('2017-10-14'); echo ceil(($date - time())/60/60/24); ?> days</td>
+        <td style="width: 160px"><?php $date = strtotime('2017-10-14'); echo ceil(($date - time()) / 60 / 60 / 24); ?> days</td>
+    </tr>
+    <tr>
+        <td>Amsterdam</td>
+        <td style="width: 160px"><?php $date = strtotime('2018-10-15'); echo ceil(($date - time()) / 60 / 60 / 24); ?> days</td>
     </tr>
     <tr>
         <td>Kazan</td>
-        <td style="width: 160px"><?php $date = strtotime('2019-08-18'); echo ceil(($date - time())/60/60/24); ?> days</td>
+        <td style="width: 160px"><?php $date = strtotime('2019-08-18'); echo ceil(($date - time()) / 60 / 60 / 24); ?> days</td>
     </tr>
 </table>
